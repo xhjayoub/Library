6c564ce25dcec579869c69e39ef9de6969b921e3
Ayoub hj <medayoubhajamor197@gmail.com>
2024-09-12 16:44:30 +0100

    Add a function that displays all books stored

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function displayBooks(bookArr) {
+  for (let book in bookArr) {
+    console.log(book);
+  }
+}
